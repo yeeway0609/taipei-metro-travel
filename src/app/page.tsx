d70dd36254499188201ec6f11100ec52028b77de
yeeway <yiwei.suuu@gmail.com>
2025-08-07 00:06:07 +0800

    add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
-import Image from "next/image"
-import { useTranslations } from "next-intl"
-import { LocaleSwitcher } from "@/components/LocaleSwitcher"
+import Image from 'next/image'
+import { useTranslations } from 'next-intl'
+import { LocaleSwitcher } from '@/components/LocaleSwitcher'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 
 export default function Home() {
-  const t = useTranslations("HomePage")
+  const t = useTranslations('HomePage')
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>{t("title")}</h1>
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+      <h1>{t('title')}</h1>
       <LocaleSwitcher />
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our
+              servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
